8898e759a01852645808a6494dd199a43ef86a9c
yangjiaxi <768107973@qq.com>
2020-02-19 22:25:39 +0800

    test

--- codes/attack.py
+++ codes/attack.py
@@ -19,14 +19,14 @@ normalize = NormalizeByChannelMeanStd(
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 model = resnet101(pretrained=True)
 model.eval()
-model = nn.Sequential(normalize, model)
+#model = nn.Sequential(normalize, model)
 model = model.to(device)
 
 #load data from validation of imagenet
 Batchsize = 1
 correct_sum = torch.zeros(17,1)
 #transform = transforms.Compose([transforms.RandomSizedCrop(224),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize(mean = [ 0.485, 0.456, 0.406 ],std = [ 0.229, 0.224, 0.225 ]),])
-transform = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224),transforms.ToTensor(),])
+transform = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
 dataset = datasets.ImageFolder('/hd2/yangjiaxi/PDG_attack_imagenet/data/Imagenet/val',transform)
 data_loader = torch.utils.data.DataLoader(dataset,batch_size = Batchsize, shuffle = False,pin_memory = True)
 for j,data in enumerate(data_loader):
@@ -34,14 +34,13 @@ for j,data in enumerate(data_loader):
 		print("iteration:",j,"complete")
 		print("correct number:",correct_sum[0],"accuracy:",float(correct_sum[0]/j))
 		print("failed attacked number:",correct_sum.view(1,17))
-	if j > 0:
+	if j > 50000:
 		break
 	cln_data, true_label = data
 	cln_data, true_label = cln_data.to(device), true_label.to(device)
 	pred_cln = predict_from_logits(model(cln_data)).to(device)
-	img = transforms.ToPILImage()(cln_data.cpu().view(3,224,224)).convert('RGB')
-	img.save('/hd2/yangjiaxi/PDG_attack_imagenet/gitupload/codes/test.jpg')
-	if pred_cln == true_label:
+	
+	if pred_cln.cuda() == true_label:
 		correct_sum[0] += 1
 		for eps in range(1,17):
 			 adversary = LinfPGDAttack(model, eps=float(eps)/255, eps_iter=float(eps)/255/40, nb_iter=40,rand_init=False, targeted=False)
